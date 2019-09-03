e2373db06555afa2456b36b42c1323847d36b597
Jonathan Frida <jonyfridja@gmail.com>
2019-09-03 11:41:56 +0300

    first-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
+import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
+import { IngredientPageComponent } from './pages/ingredient-page/ingredient-page.component';
+import { AppNavComponent } from './components/app-nav/app-nav.component';
+
+const routes = [
+  {path:'', component: RecipePageComponent},
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipePageComponent,
+    IngredientPageComponent,
+    AppNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
